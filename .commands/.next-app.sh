50af42d18ca5884084ade402b00bbd92bde54d2d
avnexcode <axnvee18@gmail.com>
2024-07-29 23:08:58 +0700

    message: update

--- .commands/.next-app.sh
+++ .commands/.next-app.sh
@@ -27,7 +27,7 @@ export default function App() {
         <div className="bg-blue-500 text-white p-4 w-full h-screen flex justify-center items-center flex-col gap-5">
             <h1 className="text-3xl font-bold uppercase">Avnexcode</h1>
             <h1 className="text-3xl font-bold"><span className="uppercase">Prot Prot</span> Builder</h1>
-            <h1 className="text-3xl font-bold">React TS Vite x TailwindCSS Builder</h1>
+            <h1 className="text-3xl font-bold">Next JS App Router x TailwindCSS Builder</h1>
         </div>
     );
 }
@@ -73,7 +73,7 @@ export default function App() {
         <div className="bg-blue-500 text-white p-4 w-full h-screen flex justify-center items-center flex-col gap-5">
             <h1 className="text-3xl font-bold uppercase">Avnexcode</h1>
             <h1 className="text-3xl font-bold"><span className="uppercase">Prot Prot</span> Builder</h1>
-            <h1 className="text-3xl font-bold">React TS Vite x TailwindCSS Builder</h1>
+            <h1 className="text-3xl font-bold">Next TS App Router x TailwindCSS Builder</h1>
         </div>
     );
 }
